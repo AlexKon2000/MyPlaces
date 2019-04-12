3b1127b82537e35efb4262c8f8b6e9333b99dd49
Alex Kondratev <myname83@list.ru>
2019-04-12 16:06:42 +0300

    Custo  Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Alex Kon on 12/04/2019.
+//  Copyright © 2019 Alex Kon. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
